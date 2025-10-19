8891c49a37082ecc601b548f560046a5d73e6d82
Miguel Angel Murillo Frias <mimurillof@unal.edu.co>
2025-10-19 00:13:07 -0500

    Include first_name and last_name in user creation

--- crud/user_service.py
+++ crud/user_service.py
@@ -24,7 +24,9 @@ class UserCRUD:
         hashed_password = get_password_hash(user.password)
         db_user = User(
             email=user.email,
-            password_hash=hashed_password
+            password_hash=hashed_password,
+            first_name=user.first_name,
+            last_name=user.last_name
         )
         db.add(db_user)
         await db.commit()
